b3f22025bdcb1090e620d3a55eed2150cc11f749
André Luiz Coelho da Silva <andre.wise@gmail.com>
2023-02-16 09:51:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,11 @@
 import streamlit
 
 streamlit.title('My Mom\'s New Healthy Dinner')
-
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
@@ -28,7 +26,6 @@ streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
 
 # Takes the JSON version and normalize it 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
